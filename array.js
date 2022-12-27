5d947f52e2c84a41b795306e7d4fda7761e1059c
VnHoe <91834973+VnHoe@users.noreply.github.com>
2022-12-27 14:01:53 +0700

    them, sua, xoa, chen, săp xep, tim kiem, lon nhat, nho nhat

--- array.js
+++ array.js
@@ -3,8 +3,8 @@ let numbers = [];
 let container = document.getElementById("array");
 let blocks = [];
 
-function generatearray(length) {
-    localStorage.clear();
+//# TẠO MẢNG NGẪU NHIÊN
+async function generatearray(length) {
     numbers = [];
     for (let i = 0; i < length; i++) {
         let value = Math.ceil(Math.random() * 100);
@@ -14,17 +14,20 @@ function generatearray(length) {
         </div>
         `
         numbers = [...numbers, value];
-        $('#array').append(div)
+        await delay(300);
+        $('#array').append(div);
     }
-
-    window.localStorage.setItem("data", JSON.stringify(numbers));
+    $('.notice').text(`Đã tạo xong mảng !!!`);
+    $('.notice').addClass('animation');
 }
 
-async function addNumber(value, position = 0) {
+//# THÊM
+async function addNumber(value) {
     let length = numbers.length;
+    let lastBlock = document.querySelector('.block:last-child');
+    let position = lastBlock ? Number(lastBlock.dataset.transform) + 40 : 0;
     numbers[length] = value;
 
-    window.localStorage.setItem("data", JSON.stringify(numbers));
     let div = `
         <div 
             class="block" 
@@ -38,9 +41,9 @@ async function addNumber(value, position = 0) {
     $('#array').append(div);
     await delay(300);
     $(".block:last-child").css({ "background-color": "#6b5b95" });
+} 
 
-}
-
+//# CHÈN 
 async function insertNumber(index, value) {
     blocks = Array.from(document.querySelectorAll(".block"));
     let length = numbers.length;
@@ -57,44 +60,41 @@ async function insertNumber(index, value) {
         numbers[i] = numbers[i - 1];
         blocks[i - 1].style.backgroundColor = "#6b5b95";
     }
-    addNumber(value, index, position);
+    addToDom(value, position);
     $('.notice').text(`Đã chèn thành công số ${value} vào vị trí số ${index}`);
     $('.notice').addClass('animation');
 
     // Chen value tai index
     numbers[index] = value;
     blocks = Array.from(document.querySelectorAll(".block"));
-    window.localStorage.setItem("data", JSON.stringify(numbers));
 }
 
+//# SỬA
 async function editNumber(index, value) {
     blocks = Array.from(document.querySelectorAll(".block"));
     let length = numbers.length;
 
     for (let i = 0; i < length; i++) {
         blocks[i].style.backgroundColor = "#FF4949";
+        await delay(300);
         if (i === index) {
             blocks[i].style.backgroundColor = "#13CE66";
-            await new Promise((resolve) =>
-                setTimeout(() => {
-                    resolve();
-                }, 300)
-            );
+            await delay(1000);
             numbers[i] = value;
+
+            blocks[i].style.height = `${value * 4}px`
             blocks[i].firstElementChild.textContent = value;
+            blocks[i].style.backgroundColor = "#6b5b95";
+
+            $('.notice').text(`Đã sửa thành công phần tử tại vị trí ${index} thành ${value} !!!`);
+            $('.notice').addClass('animation');
             return;
         }
-        await new Promise((resolve) =>
-            setTimeout(() => {
-                resolve();
-            }, 300)
-        );
         blocks[i].style.backgroundColor = "#6b5b95";
     }
-
-    window.localStorage.setItem("data", JSON.stringify(numbers));
 }
 
+//# XÓA THEO VỊ TRÍ 
 function deleteNumberByIndex(index) {
     return new Promise(async (resolve) => {
         blocks = document.querySelectorAll(".block");
@@ -117,15 +117,15 @@ function deleteNumberByIndex(index) {
             blocks[i + 1].style.backgroundColor = "#6b5b95";
         }
         numbers.length--;
-        window.localStorage.setItem("data", JSON.stringify(numbers));
     });
 
 }
 
+//# XÓA THEO GIÁ TRỊ
 async function deleteNumberByValue(value) {
     blocks = document.querySelectorAll(".block");
     if (numbers.length <= 0) {
-        alert('Mảng đang rỗng!!!')
+        return;
     }
 
     for (let i = 0; i < numbers.length; i++) {
@@ -139,26 +139,9 @@ async function deleteNumberByValue(value) {
         }
         blocks[i].style.backgroundColor = "#6b5b95";
     }
-    window.localStorage.setItem("data", JSON.stringify(numbers));
 }
 
-function swap(element1, element2) {
-    return new Promise((resolve) => {
-        // Để đổi css của hai element
-        let temp = element1.style.transform; // lưu vị trí của el1 vào temp
-        element1.style.transform = element2.style.transform;
-        element2.style.transform = temp;
-
-        window.requestAnimationFrame(function () {
-            setTimeout(() => {
-                //chèn element2 lên trước element1
-                container.insertBefore(element2, element1);
-                resolve();
-            }, 300);
-        });
-    });
-}
-
+//# SẮP XẾP
 async function BubbleSort(delay = 100) {
     blocks = document.getElementsByClassName("block");
 
@@ -198,6 +181,111 @@ async function BubbleSort(delay = 100) {
     }
     $('.notice').text(`Đã sắp xếp xong !!!`);
     $('.notice').addClass('animation');
+}
+
+//# TÌM KIẾM
+async function searchNumber(value) {
+    blocks = document.querySelectorAll(".block");
+    let indexList = [];
+    let count = 0;
+
+    for (var i = 0; i < numbers.length; i++) {
+        blocks[i].style.backgroundColor = "#FF4949";
+        await delay(300);
+        if (numbers[i] === value) {
+            count++;
+            indexList = [...indexList, i];
+            blocks[i].style.backgroundColor = "#13CE66";
+            await delay(1000);
+            $('.notice').text(`Số phần tử tìm được là: ${count} - Vị trí: [ ${indexList} ]`);
+            $('.notice').addClass('animation');
+        }
+        blocks[i].style.backgroundColor = "#6b5b95";
+    }
+}
+
+//# LỚN NHẤT
+async function maxNumber() {
+    blocks = document.querySelectorAll(".block");
+    let max = numbers[0];
+    blocks[0].style.backgroundColor = "#13CE66";
+    $('.notice').text(`Phần tử lớn nhất tạm thời là: ${max}`);
+    $('.notice').addClass('animation'); 
+
+    for (var i = 1; i < numbers.length; i++) {
+        blocks[i].style.backgroundColor = "#FF4949";
+        await delay(300);
+        if (max < numbers[i]) {
+            max = numbers[i];
+            blocks[0].style.backgroundColor = "#6b5b95"
+            blocks[i].style.backgroundColor = "#13CE66";
+            $('.notice').text(`Phần tử lớn nhất tạm thời là: ${max}`);
+            $('.notice').addClass('animation'); 
+            await delay(1000);
+        }
+        blocks[i].style.backgroundColor = "#6b5b95";
+    }
+    $('.notice').text(`Đã xong - Phần tử lớn nhất là: ${max}`);
+    $('.notice').addClass('animation'); 
+} 
+
+//# NHỎ NHẤT
+async function minNumber() {
+    blocks = document.querySelectorAll(".block");
+    let min = numbers[0];
+    blocks[0].style.backgroundColor = "#13CE66";
+    $('.notice').text(`Phần tử nhỏ nhất tạm thời là: ${min}`);
+    $('.notice').addClass('animation'); 
+
+    for (var i = 1; i < numbers.length; i++) {
+        blocks[i].style.backgroundColor = "#FF4949";
+        await delay(300);
+        if (min > numbers[i]) {
+            min = numbers[i];
+            blocks[0].style.backgroundColor = "#6b5b95"
+            blocks[i].style.backgroundColor = "#13CE66";
+            $('.notice').text(`Phần tử nhỏ nhất tạm thời là: ${min}`);
+            $('.notice').addClass('animation'); 
+            await delay(1000);
+        }
+        blocks[i].style.backgroundColor = "#6b5b95";
+    }
+    $('.notice').text(`Đã xong - Phần tử nhỏ nhất là: ${min}`);
+    $('.notice').addClass('animation'); 
+} 
+
+async function addToDom(value, position = 0) {
+    let div = `
+        <div 
+            class="block" 
+            data-transform="${position}" 
+            style="height: ${value * 4}px; 
+            transform: translate(${position}px);
+            background-color: #13CE66;"
+        >
+            <label class="element_value">${value}</label>
+        </div>`;
+    $('#array').append(div);
+    await delay(300);
+    $(".block:last-child").css({ "background-color": "#6b5b95" });
+
+}
+
+function swap(element1, element2) {
+    return new Promise((resolve) => {
+        // Để đổi css của hai element
+        let temp = element1.style.transform; // lưu vị trí của el1 vào temp
+        element1.style.transform = element2.style.transform;
+        element2.style.transform = temp;
+
+        window.requestAnimationFrame(function () {
+            setTimeout(() => {
+                //chèn element2 lên trước element1
+                container.insertBefore(element2, element1);
+                resolve();
+            }, 300);
+        });
+    });
 }
 
 async function remove(element) {
@@ -248,19 +336,26 @@ function whichtInputIsEmpty() {
         return 'index empty';
     else return 'no empty';
 }
-//********************************************************************************************************************************************** */
+//*************************************************************************** XỬ LÝ SỰ KIỆN ******************************************************************* */
 
 $('#add-btn').on('click', function (e) {
     e.preventDefault();
-    let lastBlock = document.querySelector('.block:last-child');
     let value = Number($('#value').val());
-    let posision = lastBlock ? Number(lastBlock.dataset.transform) + 40 : 0;
 
-    addNumber(value, posision);
+    if(value > 100 || value <= 0) {
+        $('.notice').text(`Giá trị không được bé hơn 1 hoặc lớn hơn 100!!!`);
+        $('.notice').addClass('animation');
+        $('#value').focus();
+        return;
+    };
+
+    addNumber(value);
+
     $('.notice').text(`Đã thêm thành công số ${value} vào mảng`);
     $('.notice').addClass('animation');
 
-    $('#value').val(null)
+    $('#value').val(null);
+    $('#value').focus();
 });
 
 $('#insert-btn').on('click', function (e) {
@@ -271,6 +366,7 @@ $('#insert-btn').on('click', function (e) {
     if (index >= numbers.length) {
         alert(`Không thể thêm vào vị trí này!!!\n*** Phải < ${numbers.length} ***`)
     }
+    
     insertNumber(index, value);
 
     $('#value').val(null);
@@ -282,13 +378,17 @@ $('#edit-btn').on('click', function (e) {
     let value = Number($('#value').val());
     let index = Number($('#index').val());
 
-    if (index > numbers.length - 1 || index < 0) {
-        alert(`Không thể sửa ở vị trí này!!!\n*** 0 < Vị trí < ${numbers.length} ***`)
+    if (index > numbers.length-1 || index < 0) {
+        $('.notice').text(`Vị trí phải >= 0 <= ${numbers.length - 1}`);
+        $('.notice').addClass('animation');
+        $('#index').focus();
+        return;
     }
 
     editNumber(index, value);
     $('#value').val(null);
     $('#index').val(null);
+    $('#index').focus();
 })
 
 $('#delete-btn').on('click', async function (e) {
@@ -326,13 +426,28 @@ $('#sort-btn').on('click', async function (e) {
     BubbleSort();
 })
 
+$('#search-btn').on('click', function (e) {
+    e.preventDefault();
+    let value = Number($('#value').val());
+
+    searchNumber(value);
+    $('#value').val(null);
+});
+
+$('#min-btn').on('click', function(e) {
+    e.preventDefault();
+    minNumber();
+});
+
+$('#max-btn').on('click', function(e) {
+    e.preventDefault();
+    maxNumber();
+})
+
 $('#refresh-btn').on('click', function (e) {
     e.preventDefault();
     $('.notice').removeClass('animation');
     container.innerHTML = '';
-    numbers = JSON.parse(window.localStorage.getItem("data"));
-    window.localStorage.clear();
-    window.localStorage.setItem("data", JSON.stringify(numbers));
 
     for (let i = 0; i < numbers.length; i++) {
         let value = numbers[i];
